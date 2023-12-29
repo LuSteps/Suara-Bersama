0147644cd531474029414ce8efb095d03ca67574
Dennis Lim <dennis313lim@gmail.com>
2023-12-29 22:32:56 +0700

    Update layout mantap

--- app/src/main/java/com/example/suarabersama/HomeAdapter.java
+++ app/src/main/java/com/example/suarabersama/HomeAdapter.java
@@ -7,10 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
@@ -29,7 +28,7 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // infalte the item Layout
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_capres_home, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_home_capres, parent, false);
         // set the view's size, margins, paddings and layout parameters
         MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
         return vh;
